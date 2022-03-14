57cacf7e54a608cb51d5e208e430fe36d43afcf6
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-15 01:54:56 +0300

    add a form to fill in new recipe

---
+++ src/app/components/addrecipe/addrecipe.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
+
+@Component({
+  selector: 'app-addrecipe',
+  templateUrl: './addrecipe.component.html',
+  styleUrls: ['./addrecipe.component.css']
+})
+export class AddrecipeComponent implements OnInit {
+
+  recipe = {
+    title: '',
+    ingredients: '',
+    method: ''
+  };
+  submitted = false;
+  constructor(private recipeService) { }
+
+  ngOnInit(): void {
+  }
+
+  saveRecipe(): void {
+    const data = {
+      title: this.recipe.title,
+      ingredients: this.recipe.ingredients,
+      method: this.recipe.method
+    };
+    this.recipeService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newRecipe(): void {
+    this.submitted = false;
+    this.recipe = {
+      title: '',
+      ingredients: '',
+      method: ''
+    }
+  }
+}
